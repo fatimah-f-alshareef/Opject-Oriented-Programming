eb04a88270018ffde379899aabd6a9e49792ed46
fatimah-f-alshareef <128916654+fatimah-f-alshareef@users.noreply.github.com>
2023-04-29 20:02:00 +0300

    Update InterfaceBill.java

---
+++ InterfaceBill.java
@@ -0,0 +1,17 @@
+
+package cafe;
+
+/**
+ *
+ * @author Donia
+ */
+public interface InterfaceBill {
+
+    double sprice = 20; 
+
+    public String informaionBill();
+
+    public String printAllDish();
+
+    public double sumOfBill();
+}
