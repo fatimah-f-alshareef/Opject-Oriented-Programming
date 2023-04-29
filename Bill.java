8c2c3cb57511cc250122023235e47e1777e96ba2
fatimah-f-alshareef <128916654+fatimah-f-alshareef@users.noreply.github.com>
2023-04-29 19:57:14 +0300

    Update Bill.java

--- Bill.java
+++ Bill.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 /**
  * Creating Bill abstract class 
  * this class imolements method in other class
- * @author BAYAN
+ * @author Fatim
  */
 public abstract class Bill implements InterfaceBill, Comparable {
 
